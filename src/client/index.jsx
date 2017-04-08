b48de072e8140284910a74bd9a4fa682ecc75b46
Manassarn Manoonchai <manassarn@gmail.com>
2017-04-08 17:08:49 +0700

    Integrate JSS

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -19,7 +19,7 @@ import setUpSocket from './socket'
 
 import App from '../shared/app'
 import helloReducer from '../shared/reducer/hello'
-import { APP_CONTAINER_SELECTOR } from '../shared/config'
+import { APP_CONTAINER_SELECTOR, JSS_SSR_SELECTOR } from '../shared/config'
 import { isProd } from '../shared/util'
 
 window.jQuery = $
@@ -58,5 +58,9 @@ if (module.hot) {
     ReactDOM.render(wrapApp(NextApp, store), rootEl)
   })
 }
+
+const jssServerSide = document.querySelector(JSS_SSR_SELECTOR)
+// flow-disable-next-line
+jssServerSide.parentNode.removeChild(jssServerSide)
 
 setUpSocket(store)
--- src/shared/config.js
+++ src/shared/config.js
@@ -16,3 +16,7 @@ export const IO_SERVER_HELLO = 'IO_SERVER_HELLO'
 
 export const APP_CONTAINER_CLASS = 'js-app'
 export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`
+
+// JSS
+export const JSS_SSR_CLASS = 'jss-ssr'
+export const JSS_SSR_SELECTOR = `.${JSS_SSR_CLASS}`
